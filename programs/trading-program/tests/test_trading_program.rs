9e7f93d6d39eee20003160d0fee3ab8a70a0ba39
SevenSwen <tihonenko.v.v@gmail.com>
2025-04-04 18:58:43 +0700

    wip

--- programs/trading-program/tests/test_trading_program.rs
+++ programs/trading-program/tests/test_trading_program.rs
@@ -1,4 +1,3 @@
-use crate::helpers::*;
 use common_tests::helpers::*;
 use common_tests::tests as common_escrow_tests;
 use cross_chain_escrow_dst_tests::tests::DstProgram;
@@ -6,26 +5,29 @@ use cross_chain_escrow_src_tests::tests::SrcProgram;
 use solana_program_test::{processor, tokio};
 use test_context::test_context;
 
+mod utils;
+
 mod test_escrow_creation_src {
     use super::*;
 
-    type TestState = TestStateTrading<SrcProgram>;
+    type TestState = utils::TestStateTrading<SrcProgram>;
 
     #[test_context(TestState)]
     #[tokio::test]
     async fn test_escrow_creation(test_state: &mut TestState) {
-        common_escrow_tests::test_escrow_creation(test_state.base).await
+        common_escrow_tests::test_escrow_creation(&mut test_state.base).await
     }
+
 }
 
-// mod test_escrow_creation_dst {
-//     use super::*;
+mod test_escrow_creation_dst {
+    use super::*;
 
-//     type TestState = TestStateTrading<DstProgram>;
+    type TestState = utils::TestStateTrading<DstProgram>;
 
-//     #[test_context(TestState)]
-//     #[tokio::test]
-//     async fn test_escrow_creation(test_state: &mut TestState) {
-//         common_escrow_tests::test_escrow_creation(test_state.base).await
-//     }
-// }
+    #[test_context(TestState)]
+    #[tokio::test]
+    async fn test_escrow_creation(test_state: &mut TestState) {
+        common_escrow_tests::test_escrow_creation(&mut test_state.base).await
+    }
+}
