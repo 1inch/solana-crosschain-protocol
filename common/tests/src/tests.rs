e108e934f590527c37257264c1e54e694e6f41f1
Sandeep.C.R <sandeep.chandrika@serokell.io>
2025-05-08 12:48:13 +0530

    Review: Simulate transaction

--- common/tests/src/tests.rs
+++ common/tests/src/tests.rs
@@ -3,7 +3,6 @@ use anchor_lang::error::ErrorCode;
 use anchor_spl::token::spl_token::{error::TokenError, state::Account as SplTokenAccount};
 use common::{constants::RESCUE_DELAY, error::EscrowError};
 use solana_program::{keccak::hash, program_error::ProgramError, program_pack::Pack};
-use solana_program_test::BanksTransactionResultWithMetadata;
 use solana_sdk::{
     signature::Signer, signer::keypair::Keypair, system_instruction::SystemError,
     transaction::Transaction,
@@ -12,7 +11,7 @@ use solana_sdk::{
 pub async fn test_escrow_creation_tx_cost<T: EscrowVariant>(test_state: &mut TestStateBase<T>) {
     // NOTE: To actually see the output from this test, use the `--show-output` flag as shown below
     // `cargo test -- --show-output` or
-    // `cargo test test_escrow_creation_tx_cost -- --show-output` or
+    // `cargo test test_escrow_creation_cost -- --show-output` or
     let (_, _, tx) = create_escrow_data(test_state);
 
     println!(
@@ -21,29 +20,16 @@ pub async fn test_escrow_creation_tx_cost<T: EscrowVariant>(test_state: &mut Tes
     );
 }
 
-async fn measure_tx_compute_units<T>(
-    test_state: &mut TestStateBase<T>,
-    mut tx: Transaction,
-) -> u64 {
+async fn measure_tx_compute_units<T>(test_state: &mut TestStateBase<T>, tx: Transaction) -> u64 {
     // retry at most 5 times.
     for _ in 0..5 {
-        let r = test_state
-            .client
-            .process_transaction_with_metadata(tx.clone())
-            .await;
-        match r {
-            Result::Ok(BanksTransactionResultWithMetadata {
-                metadata: Some(m), ..
-            }) => {
-                return m.compute_units_consumed;
-            }
-            _ => {
-                let new_hash = test_state.context.get_new_latest_blockhash().await.unwrap();
-                tx.message.recent_blockhash = new_hash;
+        if let Ok(r) = test_state.client.simulate_transaction(tx.clone()).await {
+            if let Some(m) = r.simulation_details {
+                return m.units_consumed;
             }
         }
     }
-    panic!("Failed to fetch transaction metadata!")
+    panic!("Failed to fetch simulation details!")
 }
 
 pub async fn test_escrow_creation<T: EscrowVariant>(test_state: &mut TestStateBase<T>) {
