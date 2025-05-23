14b09d575f7902eef573e2ca7a5fc00a47e658bd
Sandeep.C.R <sandeep.chandrika@serokell.io>
2025-05-23 17:21:54 +0530

    fixup! fixup! Merge remote-tracking branch 'origin/master'

--- common/tests/src/dst_program.rs
+++ common/tests/src/dst_program.rs
@@ -35,35 +35,7 @@ impl<S: TokenVariant> EscrowVariant<S> for DstProgram {
         escrow_ata: &Pubkey,
         withdrawer: &Keypair,
     ) -> Transaction {
-        let instruction_data =
-            InstructionData::data(&cross_chain_escrow_dst::instruction::PublicWithdraw {
-                secret: test_state.secret,
-            });
-
-        let instruction: Instruction = Instruction {
-            program_id: cross_chain_escrow_dst::id(),
-            accounts: vec![
-                AccountMeta::new(test_state.creator_wallet.keypair.pubkey(), false),
-                AccountMeta::new_readonly(test_state.recipient_wallet.keypair.pubkey(), false),
-                AccountMeta::new(withdrawer.pubkey(), true),
-                AccountMeta::new_readonly(test_state.token, false),
-                AccountMeta::new(*escrow, false),
-                AccountMeta::new(*escrow_ata, false),
-                AccountMeta::new(test_state.recipient_wallet.token_account, false),
-                AccountMeta::new_readonly(S::get_token_program_id(), false),
-                AccountMeta::new_readonly(system_program_id, false),
-            ],
-            data: instruction_data,
-        };
-
-        Transaction::new_signed_with_payer(
-            &[instruction],
-            Some(&test_state.payer_kp.pubkey()), // so that withdrawer does not incurr transaction
-            // charges and mess up computation of withdrawer's
-            // balance expectation.
-            &[withdrawer, &test_state.payer_kp],
-            test_state.context.last_blockhash,
-        )
+        build_public_withdraw_tx_dst(test_state, escrow, escrow_ata, withdrawer)
     }
 
     fn get_withdraw_tx(
