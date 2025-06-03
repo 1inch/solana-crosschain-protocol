38779ba3489f782f9daae7290d0a4f5bac407147
Brgndy25 <szn.wavez@gmail.com>
2025-06-04 01:38:13 +0400

    [SOL-121] feat: impl cancel_order and cancel_order_by_resolver tx builders for tests

--- common/tests/src/src_program.rs
+++ common/tests/src/src_program.rs
@@ -337,6 +337,73 @@ pub fn get_create_order_tx<T: EscrowVariant<S>, S: TokenVariant>(
     )
 }
 
+pub fn get_cancel_order_tx<T: EscrowVariant<S>, S: TokenVariant>(
+    test_state: &TestStateBase<T, S>,
+    order: &Pubkey,
+    order_ata: &Pubkey,
+) -> Transaction {
+    let instruction_data =
+        InstructionData::data(&cross_chain_escrow_src::instruction::CancelOrder {});
+
+    let (creator_ata, _) = find_user_ata(test_state);
+
+    let instruction: Instruction = Instruction {
+        program_id: cross_chain_escrow_src::id(),
+        accounts: vec![
+            AccountMeta::new(test_state.creator_wallet.keypair.pubkey(), true),
+            AccountMeta::new_readonly(test_state.token, false),
+            AccountMeta::new(*order, false),
+            AccountMeta::new(*order_ata, false),
+            AccountMeta::new(creator_ata, false),
+            AccountMeta::new_readonly(S::get_token_program_id(), false),
+            AccountMeta::new_readonly(system_program_id, false),
+        ],
+        data: instruction_data,
+    };
+
+    Transaction::new_signed_with_payer(
+        &[instruction],
+        Some(&test_state.payer_kp.pubkey()),
+        &[&test_state.payer_kp, &test_state.creator_wallet.keypair],
+        test_state.context.last_blockhash,
+    )
+}
+
+pub fn get_cancel_order_by_resolver_tx<T: EscrowVariant<S>, S: TokenVariant>(
+    test_state: &TestStateBase<T, S>,
+    order: &Pubkey,
+    order_ata: &Pubkey,
+) -> Transaction {
+    let reward_limit = test_state.test_arguments.reward_limit;
+    let instruction_data = InstructionData::data(
+        &cross_chain_escrow_src::instruction::CancelOrderByResolver { reward_limit },
+    );
+
+    let (creator_ata, _) = find_user_ata(test_state);
+
+    let instruction: Instruction = Instruction {
+        program_id: cross_chain_escrow_src::id(),
+        accounts: vec![
+            AccountMeta::new(test_state.recipient_wallet.keypair.pubkey(), true),
+            AccountMeta::new(test_state.creator_wallet.keypair.pubkey(), false),
+            AccountMeta::new_readonly(test_state.token, false),
+            AccountMeta::new(*order, false),
+            AccountMeta::new(*order_ata, false),
+            AccountMeta::new(creator_ata, false),
+            AccountMeta::new_readonly(S::get_token_program_id(), false),
+            AccountMeta::new_readonly(system_program_id, false),
+        ],
+        data: instruction_data,
+    };
+
+    Transaction::new_signed_with_payer(
+        &[instruction],
+        Some(&test_state.payer_kp.pubkey()),
+        &[&test_state.payer_kp, &test_state.recipient_wallet.keypair],
+        test_state.context.last_blockhash,
+    )
+}
+
 pub async fn create_order<S: TokenVariant>(
     test_state: &TestStateBase<SrcProgram, S>,
 ) -> (Pubkey, Pubkey) {
